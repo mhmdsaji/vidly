5e6bc9c988de625a4c45e119e6d354eeb80772f1
Mohamed Saji <mohamed.saji@viseo.com>
2021-06-18 16:15:47 +0100

    CC to FC

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -1,33 +1,31 @@
 import React, { Component } from "react";
 import _ from "lodash";
 
-class TableBody extends Component {
-  renderCell = (item, column) => {
+import React from "react";
+
+function TableBody(props) {
+  const { data, columns } = this.props;
+
+  const renderCell = (item, column) => {
     if (column.content) return column.content(item);
     return _.get(item, column.path);
   };
 
-  createKey = (item, column) => {
+  const createKey = (item, column) => {
     return item.id + (column.path || column.key);
   };
 
-  render() {
-    const { data, columns } = this.props;
-
-    return (
-      <tbody>
-        {data.map(item => (
-          <tr key={item.id}>
-            {columns.map(column => (
-              <td key={this.createKey(item, column)}>
-                {this.renderCell(item, column)}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    );
-  }
+  return (
+    <tbody>
+      {data.map((item) => (
+        <tr key={item.id}>
+          {columns.map((column) => (
+            <td key={createKey(item, column)}>{renderCell(item, column)}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
 }
 
 export default TableBody;
